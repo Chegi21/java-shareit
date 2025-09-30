acae5779b8a6a05e668f1a7363901ba9459e31ff
Chegi21 <bilev.jenia@yandex.ru>
2025-09-30 15:12:27 +0300

    init commit

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,30 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
+@Data
+@Builder
 public class ItemDto {
+    @PositiveOrZero(message = "Id не может отрицательным числом")
+    private Long id;
+
+    @NotBlank(message = "Название вещи не может быть пустым")
+    private String name;
+
+    @NotBlank(message = "Описание вещи не может быть пустым")
+    private String description;
+
+    @NotNull
+    private Boolean isAvailable;
+
+    @PositiveOrZero(message = "Id не может отрицательным числом")
+    private Long ownerId;
+
+    @PositiveOrZero(message = "Id не может отрицательным числом")
+    private Long requestId;
 }
